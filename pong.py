6d3f71d1bb7d4a7ef1590aba698609b087664870
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-01 23:50:01 +0530

    added left side paddle to game with name paddle_a

--- pong.py
+++ pong.py
@@ -6,10 +6,20 @@ import turtle
 Width = 800
 Height = 600
 
+#Window setup
 window = turtle.Screen()
 window.title("Pong Game By ankurrai1")
 window.bgcolor("black")
 window.setup(width = Width, height = Height)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
 
 while True:
     window.update()
