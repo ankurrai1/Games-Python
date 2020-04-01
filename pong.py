3e95eefc299a1df48e01f375fa61b64a59add30e
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-01 23:52:41 +0530

    added right side paddle to game with name paddle_b

--- pong.py
+++ pong.py
@@ -21,5 +21,14 @@ paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
 paddle_a.penup()
 paddle_a.goto(-350,0)
 
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_b.penup()
+paddle_b.goto(350,0)
+
 while True:
     window.update()
