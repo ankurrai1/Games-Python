3aa13ad01084bccea3c6cd689861dffbdee97cf7
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-01 23:31:05 +0530

    added window and turtle to gamea

---
+++ pong.py
@@ -0,0 +1,15 @@
+# Simple pong game in python 3
+# this game is based on turtle
+
+import turtle
+
+Width = 800
+Height = 600
+
+window = turtle.Screen()
+window.title("Pong Game By ankurrai1")
+window.bgcolor("black")
+window.setup(width = Width, height = Height)
+
+while True:
+    window.update()
