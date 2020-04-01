bc3419375ea2d6b9d4afeae063e7f1a41790af6a
Ankur Kumar Rai <rai.ankur0007@gmail.com>
2020-04-01 23:55:20 +0530

    added square pong in the center to game with name ball

--- pong.py
+++ pong.py
@@ -30,5 +30,14 @@ paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
 paddle_b.penup()
 paddle_b.goto(350,0)
 
+# ball
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+# paddle_b.shapesize(stretch_wid = 5, stretch_len = 1)
+paddle_b.penup()
+paddle_b.goto(0,0)
+
 while True:
     window.update()
